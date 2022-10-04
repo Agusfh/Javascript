9820ce54a6061169a47e726e2fd582fc08f60625
agus fernandez <fernandezhagustina@gmail.com>
2022-10-04 13:58:11 -0300

    1er entrega

---
+++ codigo.js
@@ -0,0 +1,44 @@
+
+/*products*/
+
+const fernet_price = 800;
+
+const gancia_price = 750;
+
+const whisky_price = 1300;
+
+const vermut_price = 600;
+
+/*alert inicial*/
+
+let edad = prompt ("Ingresa tu edad");
+
+if (edad >= 18){
+alert ("¡Bienvenido!" +" "+ "\nBeber con moderación - Prohibida su venta a menores de 18 años");
+}
+
+else if (edad == ""){
+    alert("Por favor, ingresa tu edad");
+}
+
+else{
+    alert("Eres menor de edad, por favor volve cuando tengas 18 años.");
+}
+
+/* alert de calculo iva y precio final */
+
+let precioprod = parseFloat (prompt ("A continuación podrás ingresar el precio del producto que deseas, para conocer su costo final con iva incluído. \nIngresá el precio del producto que prefieras: \n1 - Fernet $800 \n2 - Gancia $750 \n3 - Whisky $1300 \n4 - Vermut $600"));
+
+function calcularIva(precio){
+    return precio * 0.21;}
+
+ivaCalculado = calcularIva(precioprod);
+alert ("EL iva del producto es = $"+ivaCalculado);
+
+function calcularPrecioFinal(precio, iva){
+    return precio + iva;}
+    
+let precioFinal = calcularPrecioFinal(precioprod,ivaCalculado);
+
+alert ("El precio final del producto con iva incluido es $"+precioFinal);
+
