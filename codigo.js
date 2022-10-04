743c52eb715decd2291fa7e6cb02280e147a03d3
agus fernandez <fernandezhagustina@gmail.com>
2022-10-04 16:10:10 -0300

    cambios entrega

--- codigo.js
+++ codigo.js
@@ -43,15 +43,26 @@ let precioFinal = calcularPrecioFinal(precioprod,ivaCalculado);
 alert ("El precio final del producto con iva incluido es $"+precioFinal);
 
 
-let masprod = prompt ("¿Te gustaría ver el precio final de otro producto? si/no");
 
-if (masprod == "si") || (masprod == "SI"){
-alert ("ngresá el precio del producto que deseas, para conocer su costo final con iva incluído. \nIngresá el precio del producto que prefieras: \n1 - Fernet $800 \n2 - Gancia $750 \n3 - Whisky $1300 \n4 - Vermut $600"");
-}
+let masprod = prompt ("¿Te gustaría ver el precio final de otro producto: -si -ESC para salir");
 
-else{
-    alert("Gracias por visitar CHIN CHIN BEBIDAS");
+while (masprod != "ESC"){
+    calcularPrecio();}
 
+    if (masprod == "si"|| masprod == "SI"){
+        alert ("A continuación podrás ingresar el precio del producto que deseas, para conocer su costo final con iva incluído. \nIngresá el precio del producto que prefieras: \n1 - Fernet $800 \n2 - Gancia $750 \n3 - Whisky $1300 \n4 - Vermut $600");}
 
-si la respuesta es si... muestro menu 
+    let precioprod2 = parseFloat (prompt ("A continuación podrás ingresar el precio del producto que deseas, para conocer su costo final con iva incluído. \nIngresá el precio del producto que prefieras: \n1 - Fernet $800 \n2 - Gancia $750 \n3 - Whisky $1300 \n4 - Vermut $600"));
 
+    function calcularIva(precio){
+        return precio * 0.21;}
+    
+    ivaCalculado = calcularIva(precioprod2);
+    alert ("EL iva del producto es = $"+ivaCalculado);
+    
+    function calcularPrecioFinal(precio, iva){
+        return precio + iva;}
+        
+    let precioFinalok = calcularPrecioFinal(precioprod2,ivaCalculado);
+    
+    alert ("El precio final del producto con iva incluido es $"+precioFinalok);
