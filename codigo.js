95fd7743b73c0823cf374f4de1c089b611541936
agus fernandez <fernandezhagustina@gmail.com>
2022-10-23 20:42:33 -0300

    carrito avanzado

--- codigo.js
+++ codigo.js
@@ -18,7 +18,7 @@ alert ("¡Bienvenido!" +" "+ "\nBeber con moderación - Prohibida su venta a men
 
 /* alert de calculo iva y precio final */
 
-let precioprod = parseFloat (prompt ("A continuación podrás ingresar el precio del producto que deseas, para conocer su costo final con iva incluído. \nIngresá el precio del producto que prefieras: \n1 - Fernet $800 \n2 - Gancia $750 \n3 - Whisky $1300 \n4 - Vermut $600"));
+/*let precioprod = parseFloat (prompt ("A continuación podrás ingresar el precio del producto que deseas, para conocer su costo final con iva incluído. \nIngresá el precio del producto que prefieras: \n1 - Fernet $800 \n2 - Gancia $750 \n3 - Whisky $1300 \n4 - Vermut $600"));
 
 function calcularIva(precio){
     return precio * 0.21;}
@@ -74,7 +74,10 @@ alert("Contamos con stock de esa bebida");
 }
 else{
 alert("No contamos con stock de esa bebida, lo sentimos");
-}
+}*/ 
+
+//dejo comentado los alert para que no se muestren
+
 }
 
 //flujo edad
@@ -85,5 +88,59 @@ else if (edad == ""){
 
 else{
     alert("Eres menor de edad, por favor volve cuando tengas 18 años.");
+}
+
+
+//cards y boton dinámico para carrito
+
+const carrito = [];
+
+let contenedor = document.getElementById ("misprods");
+
+function renderizarProds(){
+    for(const producto of productos){
+        contenedor.innerHTML += `
+            <div class="card col-sm-4" style="width: 18rem;">
+                <img src=${producto.foto} class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${producto.id}</h5>
+                    <p class="card-text">${producto.nombre}</p>
+                    <p class="card-text">$ ${producto.precio}</p>
+                    <button id="btn${producto.id}" class= "btn btn-primary">Comprar</button>
+                </div>
+            </div>
+        `;
+    }
+
+//eventos
+
+productos.forEach(producto => {
+        //creo el evento para cada uno de los botones de producto
+        document.getElementById(`btn${producto.id}`).addEventListener("click",function(){
+            agregarAlCarrito(producto);
+        })
+})
+}
+
+renderizarProds();
+
+function agregarAlCarrito(productoAComprar){
+    carrito.push(productoAComprar);
+    console.table(carrito);
+    alert("Producto: "+productoAComprar.nombre+" Agregado al Carrito!");
+
+    //cuadro de carrito donde se van añadiendo los productos al html
+
+document.getElementById("tablabody").innerHTML += `
+<tr>
+    <td>${productoAComprar.id}</td>
+    <td>${productoAComprar.nombre}</td>
+    <td>${productoAComprar.precio}</td>
+</tr>
+`;
+
+let totalCarrito= carrito.reduce ((acumulador,producto) => acumulador + producto.precio,0);
+let infoTotal = document.getElementById("total");
+infoTotal.innerText = "Total a Pagar $: "+totalCarrito;
 }
 
---
+++ productos.js
@@ -0,0 +1,39 @@
+const productos= [{
+    id:1,
+    foto: "./images/fernet.jpg",
+    nombre: "Fernet",
+    precio: 800
+},
+{
+    id:2,
+    foto: "./images/gancia.png",
+    nombre: "Gancia",
+    precio: 750
+},
+{
+    id:3,
+    foto: "./images/jameson.jpg",
+    nombre: "Whisky Jameson",
+    precio: 1300
+},
+{
+    id:4,
+    foto: "./images/vermut.jpg",
+    nombre: "Vermut",
+    precio: 600
+},
+
+{
+    id:5,
+    foto: "./images/bombay.png",
+    nombre: "Gin Bombay",
+    precio: 1800
+},
+
+{
+    id:6,
+    foto: "./images/mumm.jpg",
+    nombre: "Champagne Mumm",
+    precio: 1400
+}
+];
