15e10a041a7e4d5ca4f5c1f92519557bb6f02f26
agus fernandez <fernandezhagustina@gmail.com>
2022-10-30 19:58:19 -0300

    actualización html y js

--- codigo.js
+++ codigo.js
@@ -100,7 +100,7 @@ let contenedor = document.getElementById ("misprods");
 function renderizarProds(){
     for(const producto of productos){
         contenedor.innerHTML += `
-            <div class="card col-sm-4" style="width: 18rem;">
+            <div class="card col-sm-3" style="width: 18rem;">
                 <img src=${producto.foto} class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${producto.id}</h5>
@@ -144,3 +144,55 @@ let infoTotal = document.getElementById("total");
 infoTotal.innerText = "Total a Pagar $: "+totalCarrito;
 }
 
+//DARK AND LIGHT MODE EN BODY
+
+let boton = document.getElementById("mode");
+
+let principal = document.getElementById("principal");
+
+let modo = localStorage.getItem("modo");
+
+//primer renderizado - cambio de color y guardado en storage
+
+if (modo !=null){
+    document.body.className=modo;
+    principal.className="position-relative overflow-hidden p-3 text-center"+modo;
+    if(modo== "light"){
+        boton.innerText="Dark Mode";
+    }else{
+        boton.innerText= "Light Mode";
+    }
+}else{
+    modo= "light";
+}
+
+boton.onclick = () =>{
+    if (modo == "light"){
+        document.body.className= "dark";
+        principal.classList.remove("light");
+        principal.classList.add("dark");
+        boton.innerText="Light Mode";
+        modo= "dark";
+    }else{
+        document.body.className= "light";
+        principal.classList.remove("dark");
+        principal.classList.add("light");
+        boton.innerText="Dark Mode";
+        modo= "light";
+    }
+localStorage.setItem("modo",modo);
+
+//consultar si esta ok de esta forma para que la card me cambie de color tambien
+
+if (modo == "light"){
+    document.article.className= "dark";
+    misprods.classList.remove("light");
+    misprods.classList.add("dark");
+    modo= "dark";
+}else{
+    document.article.className= "light";
+    misprods.classList.remove("dark");
+    misprods.classList.add("light");
+    modo= "light";
+}
+}
